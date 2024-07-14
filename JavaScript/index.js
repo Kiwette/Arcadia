68b92d434ce47acb48857e508f39574822c0d547
leleux <9112590p@commun.ad.sncf.fr>
2024-07-14 10:57:41 +0200

    Modification 140724

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,14 +1,3 @@
 alert("Bienvenue sur le site Arcadia");
 
-let choix = true;
-function change() {
-  let button = document.getElementById("button");
-  if (choix == true) {
-    button.style.backgroundColor = "red";
-    choix = false;
-  } 
-  else {
-    button.style.backgroundColor = "#F8F5E3";
-    choix = true;
-  }
-}
+
